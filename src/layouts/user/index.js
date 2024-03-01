dc40bc94016e700c5ee692fef471edf892aa68d5
ChauAnh19072003 <annavuducchauanh1907@gmail.com>
2024-03-01 08:09:49 +0700

    edit theme

--- src/layouts/user/index.js
+++ src/layouts/user/index.js
@@ -1,7 +1,5 @@
-// Chakra imports
 import { Portal, Box, useDisclosure, Text, Button, Link } from '@chakra-ui/react';
 import Footer from 'components/footer/FooterUser.js';
-// Layout components
 import Navbar from 'components/navbar/NavbarUser.js';
 import Sidebar from 'components/sidebar/Sidebar.js';
 import { SidebarContext } from 'contexts/SidebarContext';
@@ -9,13 +7,10 @@ import React, { useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import routes from 'routes.js';
 
-// Custom Chakra theme
 export default function Dashboard(props) {
 	const { ...rest } = props;
-	// states and functions
 	const [ fixed ] = useState(false);
 	const [ toggleSidebar, setToggleSidebar ] = useState(false);
-	// functions for changing the states from components
 	const getRoute = () => {
 		return window.location.pathname !== '/user/full-screen-maps';
 	};
