2351da81b2dd5d378e922b438bfc3e386a9e16da
ChauAnh19072003 <annavuducchauanh1907@gmail.com>
2024-03-04 18:00:55 +0700

    update category

--- src/views/user/categories/index.jsx
+++ src/views/user/categories/index.jsx
@@ -1,27 +1,130 @@
-import React, { useState, useEffect } from 'react';
-import fetchCategoriesForCurrentUser from 'services/categories.service'; 
+import React, { useEffect, useState } from "react";
+import AuthService from "services/auth.service";
+import axios from "axios";
+import {
+  Box,
+  Text,
+  useColorModeValue,
+  Flex,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import Card from "components/card/Card";
 
 const CategoryList = () => {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    const loadCategories = async () => {
-      const fetchedCategories = await fetchCategoriesForCurrentUser();
-      setCategories(fetchedCategories);
+    const fetchCategories = async () => {
+      const currentUser = AuthService.getCurrentUser();
+      if (currentUser) {
+        try {
+          const response = await axios.get(
+            `/api/categories/user/${currentUser.id}`
+          );
+          setCategories(response.data);
+        } catch (error) {
+          console.error("Error fetching categories:", error);
+        }
+      }
     };
 
-    loadCategories();
+    fetchCategories();
   }, []);
 
+  const groupedCategories = categories.reduce((acc, category) => {
+    const { type } = category;
+    if (!acc[type]) {
+      acc[type] = [];
+    }
+    acc[type].push(category);
+    return acc;
+  }, {});
+  const textColor = useColorModeValue("secondaryGray.900", "white");
   return (
-    <div>
-      <h2>Categories for Current User</h2>
-      <ul>
-        {categories.map((category) => (
-          <li key={category.id}>{category.name}</li>
+    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <Card
+        direction="column"
+        w="70%"
+        px="150px"
+        mx="auto"
+        my="auto"
+        overflowX={{ sm: "scroll", lg: "hidden" }}
+      >
+        {Object.keys(groupedCategories).map((type) => (
+          <div key={type}>
+            <Text
+              color={textColor}
+              fontSize="22px"
+              fontWeight="700"
+              lineHeight="100%"
+              display="flex"
+              alignItems="flex-start"
+            >
+              {type === "EXPENSE" ? "Expense" : "Income"} categories
+            </Text>
+            {/* <ul>
+              {groupedCategories[type].map((category) => (
+                <li key={category.id} style={{ listStyle: "none" }}>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    {category.icon && category.icon.path && (
+                      <img
+                        src={
+                          process.env.PUBLIC_URL +
+                          "/assets/img/icons/" +
+                          category.icon.path
+                        }
+                        alt={category.name + " Icon"}
+                        style={{
+                          width: "50px",
+                          height: "50px",
+                          marginRight: "10px",
+                        }}
+                      />
+                    )}
+                    <p>{category.name}</p>
+                  </div>
+                </li>
+              ))}
+            </ul> */}
+            <Table variant="simple" color="gray.500" mb="24px">
+              {groupedCategories[type].map((category) => (
+                <Tbody>
+                  <Tr key={category.id}>
+                    <Flex align="center">
+                      <Text color={textColor} display='flex' my='10px'>
+                        {category.icon && category.icon.path && (
+                          <img
+                            src={
+                              process.env.PUBLIC_URL +
+                              "/assets/img/icons/" +
+                              category.icon.path
+                            }
+                            alt={category.name + " Icon"}
+                            style={{
+                              width: "50px",
+                              height: "50px",
+                              marginRight: "10px",
+                            }}
+                          />
+                        )}
+                        <p>{category.name}</p>
+                      </Text>
+                    </Flex>
+                    <Td>Delete | Update</Td>
+                  </Tr>
+                  
+                </Tbody>
+              ))}
+            </Table>
+          </div>
         ))}
-      </ul>
-    </div>
+      </Card>
+    </Box>
   );
 };
 
